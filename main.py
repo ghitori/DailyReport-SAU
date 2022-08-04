2020c258a448f75420e2282b17de6ae2fdc83ad3
Shan_shanHY <shan_shanhy@qq.com>
2022-08-05 06:51:19 +0800

    添加python端结果输出&适配Actions

--- main.py
+++ main.py
@@ -39,6 +39,6 @@ if code == 200:
 else:
         pushmessage = ["疫情填报失败！服务器连接错误！", "**请检查服务器地址！**"]
         exit_code = -1
-print(pushmessage[0],'\n',pushmessage[1])
+print(pushmessage[0],'\n',res.text)
 req.pushdeer('', pushkey, pushmessage[0], pushmessage[1])
 exit(exit_code)
