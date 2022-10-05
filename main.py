55609accafd8b7095e4242fd27dcd8bafaf0aa6c
Bai <2496973903@qq.com>
2022-10-06 00:24:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import starryrequest as req
-import json
 import configparser
-import random
 import datetime
+import json
+import random
+
+import starryrequest as req
 
 # 加载配置文件
 file = 'config.ini'
@@ -18,33 +19,41 @@ uids = dict(con.items('Push'))['uids']
 
 # 随机体温
 if not body['tiwen']:
-        body['tiwen'] = random.choice(temp).strip()
+    body['tiwen'] = random.choice(temp).strip()
 if not body['tiwen1']:
-        body['tiwen1'] = random.choice(temp).strip()
+    body['tiwen1'] = random.choice(temp).strip()
 if not body['tiwen2']:
-        body['tiwen2'] = random.choice(temp).strip()
+    body['tiwen2'] = random.choice(temp).strip()
 
 # 发送日报信息
 url = "https://yqdwxx.sau.edu.cn/"
-res = req.sreq( f'{url}ADDJKTB', "", 'json', body, '')
+res = req.sreq(f'{url}ADDJKTB', "", 'json', body, '')
 
-#推送通知
+# 推送通知
 pushbody = f'学工号: {body["userid"]}\n姓名: {body["xingming"]}\n手机号: {body["shoujihao"]}\n单位院系: {body["dwyx"]}\n当前所在省份: {body["shengfen"]}\n所在城市: {body["chengshi"]}\n14日内是否有中高风险地区旅居史、接触史: {body["ljsjcs"]}\n是否按照要求向学校、社区及时上报: {body["sqsb"]}\n是否离沈: {body["sfls"]}\n离沈去向: {body["lsqx"]}\n是否隔离观察: {body["sfgl"]}\n是否身体有疑似典型症状: {body["sfys"]}\n是否发热: {body["sffr"]}\n其他信息: {body["other"]}\n体温(早): {body["tiwen"]}\n体温(中): {body["tiwen1"]}\n体温(晚): {body["tiwen2"]}'
 code = res.status_code
 if code == 200:
-        resbody = json.loads(res.text)
-        if resbody['status'] == 'OK':
-                pushmessage = ["疫情填报成功！" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), f"**填报信息**\n---\n```\n{pushbody}\n```"]
-                exit_code = 0
-        else:
-                pushmessage = ["疫情填报失败！" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), f"**请检查配置并手动填报！**\n\n**服务器返回信息：{res.text}**\n\n**填报信息**\n---\n```\n{pushbody}\n```"]
-                exit_code = -1
-else:
-        pushmessage = ["疫情填报失败！服务器连接错误！" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), "**请检查服务器地址！**"]
+    resbody = json.loads(res.text)
+    if resbody['status'] == 'OK':
+        pushmessage = ["疫情填报成功！" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
+                       f"**填报信息**\n---\n```\n{pushbody}\n```"]
+        exit_code = 0
+    else:
+        pushmessage = ["疫情填报失败！" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
+                       f"**请检查配置并手动填报！**\n\n**服务器返回信息：{res.text}**\n\n**填报信息**\n---\n```\n{pushbody}\n```"]
         exit_code = -1
-print(pushmessage[0],'\n',res.text,'\n','具体信息请查看pushdeer或WxPusher内推送')
-req.wxpusher(apptoken=apptoken, content=pushmessage[1], summary=pushmessage[0], uids=uids)
-req.pushdeer('', pushkey, pushmessage[0], pushmessage[1])
-with open('config.ini', 'r') as f:
-        req.pushdeer('', pushkey, '信息', f.read())
-exit(exit_code)
+else:
+    pushmessage = ["疫情填报失败！服务器连接错误！" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), "**请检查服务器地址！**"]
+    exit_code = -1
+print(pushmessage[0], '\n', res.text, '\n')
+if apptoken != '':
+    req.wxpusher(apptoken=apptoken, content=pushmessage[1], summary=pushmessage[0], uids=uids)
+    print('具体信息请查看WxPusher内推送')
+
+if pushkey != '':
+    req.pushdeer('', pushkey, pushmessage[0], pushmessage[1])
+    print('具体信息请查看pushdeer内推送')
+
+# with open('config.ini', 'r') as f:
+#     req.pushdeer('', pushkey, '信息', f.read())
+# exit(exit_code)
